4d836c4f1e6f51fc64b0db6bc5a2fda16be1fb98
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-04-16 21:43:04 +0900

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+let date = new Date(new Date - 30 * 1000);
+let diff = new Date - date;
+
+console.log(diff/1000)
+function formatDate{
+    if (diff <= 30){
+        console.log("прямо сейчас")
+    if else(diff >= 31 && <= 300){
+        console.log("30 сек. назад")
+    }
+    if else(diff >= 301 &&  <= 345600 )
+} else console.log('вчерашняя дата');
+
+}
+
+// "прямо сейчас"
+
+//console.log(formatDate(new Date(new Date - 30 * 1000))); 
+// "30 сек. назад"
+
+//console.log(formatDate(new Date(new Date - 5 * 60 * 1000))); 
+// "5 мин. назад"
+
+// вчерашняя дата вроде 31.11.2022, 20:00
+//console.log(formatDate(new Date(new Date - 86400 * 4 * 1000)));
+//formatDate(date)
