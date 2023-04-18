0746addcba83c80915bce7adb5f8bc5f8fa4813b
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-04-18 17:23:59 +0900

    update

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-let date = new Date(new Date - 30 * 1000);
-let diff = new Date - date;
-
-diff = diff/1000;
-function formatDate{
-    if (diff <= 30){
-        console.log("прямо сейчас");
-    if else (diff >= 31 || <= 300){
-        console.log("30 сек. назад");
+//функция для формирования даты/времени в строку
+function formatDate(date){
+    let diff = (new Date - date)/1000;//разница между текущей датой и переданной датой в секундах
+    if (diff < 1 ){
+        return"прямо сейчас";
+    } //если разница меньше 1 секунды, то возвращаем "прямо сейчас"
+    else if (diff > 60){
+        return `${Math.floor(diff)} сек. назад`; // если разница меньше 1 минуты, то возвращаем количество секунд, прошедших с момента переданной даты
     }
-    if else(diff >= 301 || <= 345600 )
-} else console.log('вчерашняя дата');
-
-}
-
-// "прямо сейчас"
+    else if (diff < 3600) {
+        return `${Math.floor(diff)/60} мин. назад`; // если разница меньше 1 часа, то возвращаем количество минут, прошедших с момента переданной даты
+    }
+else {
+    let day = date.getDate().toString().padStart(2,"0");
+    let month = (date.getMonth() + 1).toString().padStart(2,"0");
+    let year = date.getFullYear().toString().slice(-2);
+    let hours = date.getHours().toString().padStart(2,"0");
+    let minutes = date.getMinutes().toString().padStart(2,"0");
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
+}}
 
 //console.log(formatDate(new Date(new Date - 30 * 1000))); 
 // "30 сек. назад"
